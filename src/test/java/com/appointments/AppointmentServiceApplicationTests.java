1aed67bcada44e5ff762ad00054a5cdb4a6c40a1
ArminNrz <arminnorozian@gmail.com>
2021-09-29 20:08:28 +0330

    Add project

---
+++ src/test/java/com/appointments/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appointments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
